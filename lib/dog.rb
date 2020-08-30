ee8fb48a3f07c38c7f618040e23e8abec604d82e
Aymes <aysikh@Aymess-MacBook-Pro.local>
2020-08-30 14:06:42 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+
+class Dog
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save # with the method save, we don't need to repeat @@all << self 
+        #we just need to call the method in initialize
+    end
+
+    def self.all
+        return @@all
+    end
+
+    def self.clear_all
+        return @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.map{|dog| dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+end
